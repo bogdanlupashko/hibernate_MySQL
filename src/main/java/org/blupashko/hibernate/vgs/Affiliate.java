e951d6a38a72e1bf2a5b8251e61dc45ccf260762
bogdanlupashko <bogdanlupashko@gmail.com>
2015-03-26 00:12:52 +0200

    initial

--- src/main/java/org/blupashko/hibernate/vgs/Affiliate.java
+++ src/main/java/org/blupashko/hibernate/vgs/Affiliate.java
@@ -3,6 +3,7 @@ package org.blupashko.hibernate.vgs;
 /**
  * Created by blupashko on 25.03.2015.
  */
+
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Id;
@@ -17,6 +18,28 @@ import javax.persistence.Column;
 public class Affiliate {
     private Integer id;
     private String name;
+    private String address;
+    private String phone;
+    private String contactName;
+    private Integer comission;
+    private String code;
+    private String adress;
+    private Integer commision;
+
+    public Affiliate() {
+    }
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     @Column(name = "name")
     public String getName() {
@@ -27,45 +50,6 @@ public class Affiliate {
         this.name = name;
     }
 
-    private String address;
-    private String phone;
-    private String contactName;
-    private Integer comission;
-    private String code;
-    private String adress;
-    private Integer commision;
-@Column(name = "adress")
-    public String getAdress() {
-        return adress;
-    }
-
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
-@Column(name = "commision")
-    public Integer getCommision() {
-        return commision;
-    }
-
-    public void setCommision(Integer commision) {
-        this.commision = commision;
-    }
-
-    public Affiliate() {
-    }
-
-    @Id
-    @GeneratedValue(generator="increment")
-    @GenericGenerator(name="increment", strategy = "increment")
-    @Column(name="id")
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     @Column(name = "address")
     public String getAddress() {
         return address;
@@ -74,6 +58,7 @@ public class Affiliate {
     public void setAddress(String address) {
         this.address = address;
     }
+
     @Column(name = "phone")
     public String getPhone() {
         return phone;
@@ -82,14 +67,16 @@ public class Affiliate {
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     @Column(name = "contactName")
     public String getContactName() {
         return contactName;
     }
 
-    public void setContactName(String contacctName) {
-        this.contactName = contacctName;
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
     }
+
     @Column(name = "comission")
     public Integer getComission() {
         return comission;
@@ -98,6 +85,7 @@ public class Affiliate {
     public void setComission(Integer comission) {
         this.comission = comission;
     }
+
     @Column(name = "code")
     public String getCode() {
         return code;
@@ -105,5 +93,23 @@ public class Affiliate {
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    @Column(name = "adress")
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Column(name = "commision")
+    public Integer getCommision() {
+        return commision;
+    }
+
+    public void setCommision(Integer commision) {
+        this.commision = commision;
     }
 }
